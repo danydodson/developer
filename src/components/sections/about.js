b8eef57bda331c2e2d78cda257cbcd4608837042
Dany Dodson <danydodson@gmail.com>
2021-05-22 03:11:08 -0500

    removed defensive code

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -5,6 +5,88 @@ import { srConfig } from '@config'
 import sr from '@utils/sr'
 import { usePrefersReducedMotion } from '@hooks'
 
+const About = () => {
+  const revealContainer = useRef(null)
+  const prefersReducedMotion = usePrefersReducedMotion()
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return
+    }
+
+    sr.reveal(revealContainer.current, srConfig())
+  }, [])
+
+  const skills = [
+    'JavaScript (ES6+)',
+    'React',
+    'Eleventy',
+    'Vue',
+    'Node.js',
+    'WordPress',
+  ]
+
+  return (
+    <StyledAboutSection id='about' ref={revealContainer}>
+      <h2 className='numbered-heading'>About Me</h2>
+
+      <div className='inner'>
+        <StyledText>
+          <div>
+            <p>
+              Hello! My name is Brittany and I enjoy creating things that live
+              on the internet. My interest in web development started back in
+              2012 when I decided to try editing custom Tumblr themes — turns
+              out hacking together a custom reblog button taught me a lot about
+              HTML &amp; CSS!
+            </p>
+
+            <p>
+              Fast-forward to today, and I&apos;ve had the privilege of working
+              at <a href='https://us.mullenlowe.com/'>an advertising agency</a>,{' '}
+              <a href='https://starry.com/'>a start-up</a>,{' '}
+              <a href='https://www.apple.com/'>a huge corporation</a>, and{' '}
+              <a href='https://scout.camd.northeastern.edu/'>
+                a student-led design studio
+              </a>
+              . My main focus these days is building accessible, inclusive
+              products and digital experiences at{' '}
+              <a href='https://upstatement.com/'>Upstatement</a> for a variety
+              of clients.
+            </p>
+
+            <p>
+              Here are a few technologies I&apos;ve been working with recently:
+            </p>
+          </div>
+
+          <ul className='skills-list'>
+            {/* {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)} */}
+            {skills.map((skill, i) => (
+              <li key={i}>{skill}</li>
+            ))}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className='wrapper'>
+            <StaticImage
+              className='img'
+              src='../../images/me.jpg'
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt='Headshot'
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  )
+}
+
+export default About
+
 const StyledAboutSection = styled.section`
   max-width: 900px;
 
@@ -112,82 +194,3 @@ const StyledPic = styled.div`
     }
   }
 `
-
-const About = () => {
-  const revealContainer = useRef(null)
-  const prefersReducedMotion = usePrefersReducedMotion()
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return
-    }
-
-    sr.reveal(revealContainer.current, srConfig())
-  }, [])
-
-  const skills = [
-    'JavaScript (ES6+)',
-    'React',
-    'Eleventy',
-    'Vue',
-    'Node.js',
-    'WordPress',
-  ]
-
-  return (
-    <StyledAboutSection id='about' ref={revealContainer}>
-      <h2 className='numbered-heading'>About Me</h2>
-
-      <div className='inner'>
-        <StyledText>
-          <div>
-            <p>
-              Hello! My name is Brittany and I enjoy creating things that live
-              on the internet. My interest in web development started back in
-              2012 when I decided to try editing custom Tumblr themes — turns
-              out hacking together a custom reblog button taught me a lot about
-              HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I&apos;ve had the privilege of working
-              at <a href='https://us.mullenlowe.com/'>an advertising agency</a>,{' '}
-              <a href='https://starry.com/'>a start-up</a>,{' '}
-              <a href='https://www.apple.com/'>a huge corporation</a>, and{' '}
-              <a href='https://scout.camd.northeastern.edu/'>
-                a student-led design studio
-              </a>
-              . My main focus these days is building accessible, inclusive
-              products and digital experiences at{' '}
-              <a href='https://upstatement.com/'>Upstatement</a> for a variety
-              of clients.
-            </p>
-
-            <p>
-              Here are a few technologies I&apos;ve been working with recently:
-            </p>
-          </div>
-
-          <ul className='skills-list'>
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
-        </StyledText>
-
-        <StyledPic>
-          <div className='wrapper'>
-            <StaticImage
-              className='img'
-              src='../../images/me.jpg'
-              width={500}
-              quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF']}
-              alt='Headshot'
-            />
-          </div>
-        </StyledPic>
-      </div>
-    </StyledAboutSection>
-  )
-}
-
-export default About
